b5500d091c8a4bc0cde65d6991bfa3d93a85b3bf
Luxe17 <hanson1717@live.com>
2022-01-29 13:21:38 -0700

    Change footer position

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer(){
     return (
-        <div>
+        <div className="footer-container">
             <ul className="footer">
                 <li className="footer-text">ABOUT</li>
                 <li className="footer-text">NEWS</li>
